5f0272cc136ebebfd634a4433b6fa733bf335ea2
CrissRivas <cristopher_rivas@outlook.es>
2024-06-22 10:29:52 -0600

    Add(GraphQL): Add GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ProductsModule } from './products/products.module';
+import { GraphQLModule } from '@nestjs/graphql';
 @Module({
-  imports: [],
+  imports: [GraphQLModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
